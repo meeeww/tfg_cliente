65d0be178e1f8a8eb4a21d5d660b94cccb0d3647
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-04-28 20:13:39 +0200

    Comienzo del componente informacion de la pagina principal

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 import MainLayout from "../layout/MainLayout";
 import '../estilos/estilos.css'
-import Slider1 from '../assets/Slider1.jpg'
-import Slider2 from '../assets/Slider2.jpg'
-import Slider3 from '../assets/Slider3.jpg'
-import Slider4 from '../assets/Slider4.jpg'
+import Carrusel from '../components/Carousel.jsx'
 
 function Inicio() {
 
@@ -13,14 +10,8 @@ function Inicio() {
   return (
     <MainLayout laPagina={nombrePagina}>
       <div className="mainBodyInicio">
-          <div className="wrapper">
-            <div className="carrusel">
-                <img src={Slider1} alt="" />
-                <img src={Slider2} alt="" />
-                <img src={Slider3} alt="" />
-                <img src={Slider4} alt="" />
-            </div>
-          </div>
+          <Carrusel/>
+          
       </div>
     </MainLayout>
   )
