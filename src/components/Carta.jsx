4670fe533d6b54a1ad498417f92715565b7293c7
Diego <a24957@svalero.com>
2023-05-04 19:54:24 +0200

    Inicio pagina Carta

---
+++ src/components/Carta.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../layout/MainLayout'
+
+const Carta = () => {
+    return(
+        <MainLayout>
+
+        </MainLayout>
+    )
+}
+
+export default Carta;
