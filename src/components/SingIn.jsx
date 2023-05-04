9c98457a3f5c730e36bac1c539982e4ef523283e
meeeww <thezasm03@gmail.com>
2023-05-04 18:42:36 +0200

    Sign in terminado

--- src/components/SingIn.jsx
+++ src/components/SingIn.jsx
@@ -1,40 +1,42 @@
-import  '../estilos/estilos.css'
-import React from 'react'
+import '../estilos/estilos.css'
 import logo from '../assets/Logo.png'
+import MainLayout from '../layout/MainLayout'
 
 
 const FormInput = () => {
     return (
-        <div className="SingInContainer">
-            <div className="SingInCabecera">
-                <img src={logo} alt="" />
+        <MainLayout>
+            <div className="SingInContainer">
+                <div className="SingInCabecera">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="SingInMedio">
+                    <a href="/signin">Sign In</a>
+                    <a href="/signup">Sign Up</a>
+                </div>
+                <div className="SingInForm">
+                    <form action="">
+                        <div>
+                            <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+                        </div>
+                        <div>
+                            <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+                        </div>
+                        <div className="staySignedIn">
+                            <label>
+                                <input type="checkbox" id="cbox1" value="first_checkbox" /><p>Stay Signed In</p>
+                            </label>
+                        </div>
+                        <div className="SingInSing">
+                            <button>Sign in</button>
+                        </div>
+                        <div className="forgotPassword">
+                            <p>Forgot your password</p>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="SingInMedio">
-                <p>Sing In</p>
-                <p>Sing Up</p>
-            </div>
-            <div className="SingInForm">
-                <form action="">
-                    <div>
-                        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
-                    </div>
-                    <div>
-                        <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-                    </div>
-                    <div>
-                        <label>
-                            <input type="checkbox" id="cbox1" value="first_checkbox"/>Stay signed in
-                        </label>
-                    </div>
-                    <div className="SingInSing">
-                       <button>Sing in</button>
-                    </div>
-                    <div>
-                        <p>Forgot your password</p>
-                    </div>
-                </form>
-            </div>
-        </div>
+        </MainLayout>
     )
 }
 
