bbba66aaa27860a3d36cfaada92b77b92ae26fa3
Diego <a24957@svalero.com>
2023-05-11 13:48:08 +0200

    Pagina ContactUs mejorada

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,7 +5,38 @@ import logo from '../assets/Logo.png'
 const ContactUs = () => {
     return (
         <MainLayout>
-            <div class="ContactUsContenedor">
+            <div className="ContactUsContenedor">
+                <div className="ContactUsContenedorDivFormulario">
+                    <div className="ContactUsContenedorDivFormularioDerecha">
+                        <div className="ContactUsContenedorFormularioInfo">
+                        <h1>Get in touch</h1>
+                            <p>At our cafeteria, we are passionate about offering you an exceptional experience and a welcoming environment where you can enjoy delicious drinks and delicious snacks. We value your comments, questions and suggestions, so we invite you to contact us at any time.</p>
+                            <p><b>Telephone:</b> Call us at the number +1 555-123-4567 and we will be happy to assist you personally.</p>
+                            <p><b>Email:</b> If you prefer to communicate in writing, send us an email to info@thecoffeetub.com and we will respond as soon as possible.</p>
+                            <p><b>Social networks:</b> Follow us on our social networks (Facebook, Instagram and Twitter) to keep up to date with our promotions, special events and news. In addition, you can send us direct messages through these platforms, and we will respond promptly.</p>
+                        </div>
+                    </div>
+                    <div className="ContactUsContenedorDivFormularioIzquierda">
+                        <div className="ContactUsFormularioDiv">
+                            <div className="ContactUsFormulario">
+                                <div className="ContactUsFormularioDcho">
+                                    <h1>Contact us</h1>
+                                    <div className="ContactUsFormularioCuerpo">
+                                        <div className="ContactUsFormularioMensajes">
+                                            <input type="text" placeholder="Full name"/>
+                                            <input type="text" placeholder="E-mail"/>
+                                            <input type="text" placeholder="Phone"/>
+                                        </div>
+                                        <div className="ContactUsFormularioMensajeGrande">
+                                            <input type="text" placeholder="Message..." />
+                                            <button className="ContactUsFormularioBoton">Send</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="ContactUsUbicacion">
                     <div className="ContactUsUbicacionDiv1">
                         <h1>New York</h1>
@@ -18,7 +49,7 @@ const ContactUs = () => {
                         </div>
                         <div className="ContactusUbicacionDiv2">
                             <i class="fa-solid fa-phone"></i>
-                            <p>976121212</p>
+                            <p>+1 555-987-6543</p>
                         </div>
                     </div>
                     <div className="ContactUsUbicacionDiv1">
@@ -32,31 +63,7 @@ const ContactUs = () => {
                         </div>
                         <div className="ContactusUbicacionDiv2">
                             <i className="fa-solid fa-phone"></i>
-                            <p>976121212</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="ContactUsFormularioDiv">
-                    <div className="ContactUsFormulario">
-                        <div className="ContactUsFormularioIzq">
-                            <div className="ContactUsFormularioImagen">
-                                <img src={logo} alt="logo" />
-                            </div>
-                        </div>
-                        <div className="ContactUsFormularioDcho">
-                            <h1>Contact us</h1>
-                            <div className="ContactUsFormularioCuerpo">
-                                <div className="ContactUsFormularioMensajes">
-                                    <input type="text" placeholder="Full name"/>
-                                    <input type="text" placeholder="E-mail"/>
-                                    <input type="text" placeholder="Phone"/>
-                                </div>
-                                <div className="ContactUsFormularioMensajeGrande">
-                                    <input type="text" placeholder="Message..."/>
-                                    <button className="ContactUsFormularioBoton">Send</button>
-                                </div>
-                            </div>
+                            <p>+1 555-246-8139</p>
                         </div>
                     </div>
                 </div>
