66bfee52199abb09462cf0129b23c9b3fd47d242
Diego <a24957@svalero.com>
2023-05-11 19:49:48 +0200

    Pagina WorkWithUs terminada

---
+++ src/components/WorkWithUsOffices.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from '../layout/MainLayout';
+import Imagen from "../components/WorkWithUsImagen";
+const WorkWithUsOffices = () => {
+    return(
+        <div className="">
+
+        </div>
+    )
+}
+
+export default WorkWithUsOffices;
