c5ee129a3500863edaa780b1b7104fffb52c994d
Diego <a24957@svalero.com>
2023-05-15 20:57:01 +0200

    Actualizada pagina WorkWithUsForm

--- src/components/WorkWithUsForm.jsx
+++ src/components/WorkWithUsForm.jsx
@@ -5,49 +5,55 @@ const WorkWithUsForm = () => {
     return (
         <MainLayout>
             <Imagen></Imagen>
-            <div className="WorkWithUsFormContenedor">
-                <div className="WorkWithUsFormDivTexto">
-                    <h1>Work with us</h1>
-                </div>
-                <div className="WorkWithUsFormFormContenedor">
-                    <div className="WorkWithUsForm1">
-                        <input type="text" placeholder="NAME" />
-                        <input type="text" placeholder="FIRST LAST NAME" />
-                        <input type="text" placeholder="SECOND LAST NAME" />
-                        <input type="text" placeholder="SOCIAL SECURITY CARD" />
-                        <input type="text" placeholder="PHONE" />
-                        <input type="text" placeholder="EMAIL" />
-                        <input type="text" placeholder="ZIP CODE" />
-                        <input type="text" placeholder="CITY" />
-                        <input type="text" placeholder="COUNTY" />
-                        <input type="text" placeholder="STATE" />
-                        <p>Job position requested</p>
-                        <div className="WorkWithUsFormCheckBoxContainer">
-                            <div className="WorkWithUsFormCheckBox">
-                                <input type="radio" name="radio" />
-                                <p>Cook</p>
+            <div className="WorkWithUsFormFondo">
+                <div className="WorkWithUsFormContenedor">
+                    <div className="WorkWithUsFormDivTexto">
+                        <h1>Work <span className="WorkWithUsColor">With Us</span> </h1>
+                    </div>
+                    <div className="WorkWithUsFormFormContenedor">
+                        <div className="WorkWithUsForm1">
+                            <input type="text" placeholder="Name" />
+                            <input type="text" placeholder="First Last Name" />
+                            <input type="text" placeholder="Second Last Name" />
+                            <input type="text" placeholder="Social Security Card" />
+                            <input type="text" placeholder="Phone" />
+                            <input type="text" placeholder="Email" />
+                            <input type="text" placeholder="Zip Code" />
+                            <input type="text" placeholder="City" />
+                            <input type="text" placeholder="County" />
+                            <input type="text" placeholder="State" />
+                            <p className="WorkWithUsTitulo"><span className="WorkWithUsColor">J</span>ob <span className="WorkWithUsColor">P</span>osition <span className="WorkWithUsColor">R</span>equested</p>
+                            <div className="WorkWithUsFormCheckBoxContainer">
+                                <div className="WorkWithUsFormCheckBox">
+                                    <input type="radio" name="radio" />
+                                    <p className="WorkWithUsTexto">Web Developer</p>
+                                </div>
+                                <div className="WorkWithUsFormCheckBox">
+                                    <input type="radio" name="radio" />
+                                    <p className="WorkWithUsTexto">Cook</p>
+                                </div>
+                                <div className="WorkWithUsFormCheckBox">
+                                    <input type="radio" name="radio" />
+                                    <p className="WorkWithUsTexto">Waiter</p>
+                                </div>
+                                <div className="WorkWithUsFormCheckBox">
+                                    <input type="radio" name="radio" />
+                                    <p className="WorkWithUsTexto">Any</p>
+                                </div>
                             </div>
-                            <div className="WorkWithUsFormCheckBox">
-                                <input type="radio" name="radio" />
-                                <p>Waiter</p>
+                            <div className="WorkWithUsFormFecha">
+                                <label className="WorkWithUsTitulo" for="birthdate"><span className="WorkWithUsColor">D</span>ate <span className="WorkWithUsColor">O</span>f <span className="WorkWithUsColor">B</span>irth</label>
+                                <input type="date" id="birthdate" name="birthdate" />
                             </div>
-                            <div className="WorkWithUsFormCheckBox">
-                                <input type="radio" name="radio" />
-                                <p>Any</p>
+                            <div className="WorkWithUsFormCheckBoxContainer">
+                                <div className="WorkWithUsFormCheckBox">
+                                    <input type="checkbox" id="miCheckbox" />
+                                    <label className="WorkWithUsTexto" for="miCheckbox">I accept the Privacy Policy</label>
+                                </div>
                             </div>
-                        </div>
-                        <div className="WorkWithUsFormFecha">
-                            <label for="birthdate">Date of birth</label>
-                            <input type="date" id="birthdate" name="birthdate" />
-                        </div>
-                        <div className="WorkWithUsFormCheckBoxContainer">
-                            <div className="WorkWithUsFormCheckBox">
-                                <input type="checkbox" id="miCheckbox" />
-                                <label for="miCheckbox">I accept the Privacy Policy</label>
+                            <div className="WorkWithUsFormBoton">
+                                <button>Send</button>
                             </div>
-                        </div>
-                        <div className="WorkWithUsFormBoton">
-                            <button>Send</button>
                         </div>
                     </div>
                 </div>
--- src/components/WorkWithUsImagen.jsx
+++ src/components/WorkWithUsImagen.jsx
@@ -11,7 +11,7 @@ const Breadcumb = () => {
                     <h1>The &<h1 style={{ color: mainColor }}>C</h1>offee Tub</h1>
                 </div>
                 <div className="subtituloBread">
-                    <h2>The job you were waiting for</h2>
+                    <h2>The <spam className="WorkWithUsColor">J</spam>ob <spam className="WorkWithUsColor">Y</spam>ou <spam className="WorkWithUsColor">W</spam>ere <spam className="WorkWithUsColor">W</spam>aiting For</h2>
                 </div>
             </div>
         </div>
