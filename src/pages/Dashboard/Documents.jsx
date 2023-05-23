432eda32b7f576b7fea7404238ed24b6ffca5564
meeeww <thezasm03@gmail.com>
2023-05-24 01:53:49 +0200

    Empezando documentos

--- src/pages/Dashboard/Documents.jsx
+++ src/pages/Dashboard/Documents.jsx
@@ -1,6 +1,5 @@
 import Axios from 'axios'
 import { useState, useEffect } from 'react';
-import '../../estilos/estilos.css'
 import Panel from '../../components/Dashboard/Panel'
 import Header from '../../components/Dashboard/Header'
 
@@ -8,9 +7,7 @@ import Header from '../../components/Dashboard/Header'
 
 function Orders() {
 
-    const [pedidos, setPedidos] = useState();
-
-    let baseURL = "http://localhost:4000/API/pedidos/consultar";
+    let baseURL = "http://localhost:4000/API/usuarios/consultar";
 
     let config = {
         timeout: 10000,
@@ -20,17 +17,6 @@ function Orders() {
     const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
-        Axios.get(baseURL, config)
-            .then((res) => {
-                setPedidos(res.data)
-                //localStorage.setItem("token", res.data)
-                //location.replace("http://localhost:5173/login")
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({ title: "this was a success" }),
-                };
-            })
-
         if (localStorage.getItem("token") != null || localStorage.getItem("token") == "") {
             Axios.get("http://localhost:4000/API/sesiones/buscar?token=" + localStorage.getItem("token")).then(response => {
                 if (response.data[0]) {
@@ -66,25 +52,10 @@ function Orders() {
                         </div>
                         <Header />
                     </div>
-                    <div className="panelInfoDashboardProducts">
-                        <div className="reportsOverviewDashboard">
-                            <h2>Order List</h2>
-                            {
-                                pedidos &&
-                                pedidos.map((item, index) => (
-                                    <div key={item.id_usuario + "-" + index} className="mainOrdersUserDashboard">
-                                        <div>
-                                            <h4>Order ID: {item.numero_pedido}</h4>
-                                            <p>Total Price: {"$" + item.preciototal}</p>
-                                        </div>
-                                        <div>
-                                            <p>Adress: {item.direccion_envio}</p>
-                                        </div>
-                                        <div>
-                                            <a href={"http://localhost:5173/user/orders/orderid?id=" + item.numero_pedido}>List of Products</a>
-                                        </div>
-                                    </div>
-                                ))}
+                    <div className="dashboardDocuments">
+                        <div className="occupationalHazardsDocuments">
+                            <h1>Texto</h1>
+                            <video src='' autoPlay={false}></video>
                         </div>
                     </div>
                 </div>
