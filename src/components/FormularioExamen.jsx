9bd6241e4e6287c4e8c46657a16042f417c100d1
Diego <a24957@svalero.com>
2023-05-31 19:41:56 +0200

    Empiezo examen

---
+++ src/components/FormularioExamen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FormularioExamen = () =>{
+    return(
+        <div className="FormularioExamenContainer">
+            <div className="FormularioExamenMenu">
+                <div className="FormularioExamenCaja">
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default FormularioExamen
