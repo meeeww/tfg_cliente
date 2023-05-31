82dadc63b560bda9f083850c59ec7ae0c2615366
Diego <a24957@svalero.com>
2023-05-31 20:31:53 +0200

    Responsive hecho

--- src/components/FormularioExamen.jsx
+++ src/components/FormularioExamen.jsx
@@ -1,11 +1,50 @@
 import React from "react";
+import { matchPath } from "react-router-dom";
+import foto1 from "../assets/Examen/capuccino.jpg"
+import foto2 from "../assets/Examen/matcha.jpg"
+import foto3 from "../assets/Examen/frapuccino.jpg"
+import foto4 from "../assets/Examen/chocolate.jpg"
 
-const FormularioExamen = () =>{
-    return(
+
+
+
+
+
+const FormularioExamen = () => {
+    return (
         <div className="FormularioExamenContainer">
             <div className="FormularioExamenMenu">
                 <div className="FormularioExamenCaja">
-                    <img src="" alt="" />
+                    <img src={foto1} alt="capuccino" />
+                    <div className="FormularioExamenCajaTexto">
+                        <h1>Café expreso</h1>
+                        <p>Bebidas con los mejores granos de cafe 100% arabica, cuidadosamente seleccionados y tostados al más puro estilo Starbucks©.</p>
+                        <button>Añadir</button>
+                    </div>
+                </div>
+                <div className="FormularioExamenCaja">
+                    <img src={foto2} alt="capuccino" />
+                    <div className="FormularioExamenCajaTexto">
+                        <h1>Té matcha</h1>
+                        <p>Diferentes mezclas de té Teavana© para tus mejores infusiones.</p>
+                        <button>Añadir</button>
+                    </div>
+                </div>
+                <div className="FormularioExamenCaja">
+                    <img src={foto3} alt="capuccino" />
+                    <div className="FormularioExamenCajaTexto">
+                        <h1>Frapuccino</h1>
+                        <p>Bebidas con los mejores granos de cafe 100% arabica, cuidadosamente seleccionados y tostados al más puro estilo Starbucks©.</p>
+                        <button>Añadir</button>
+                    </div>
+                </div>
+                <div className="FormularioExamenCaja">
+                    <img src={foto4} alt="capuccino" />
+                    <div className="FormularioExamenCajaTexto">
+                        <h1>Café expreso</h1>
+                        <p>Bebidas con los mejores granos de cafe 100% arabica, cuidadosamente seleccionados y tostados al más puro estilo Starbucks©.</p>
+                        <button>Añadir</button>
+                    </div>
                 </div>
             </div>
         </div>
